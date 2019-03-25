e209e59f3614ac407a55ee584f45e3f608bd3d3e
Paterson <a_paterson@tss-hr420-dm12.fc.ca>
2019-03-25 13:29:35 -0400

    tween experiment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 // JS file
 // 
-console.log( fired);
+// console.log( fired);
+
+(() => {
+
+	const 	badge = document.querySelector('img'),
+			footerText = document.querySelector('h3');
+
+	function animImage() {
+		TweenMax.to(badge, 1.2, {scaleY: 1.025, scaleY: 1.025, rotation: 50, ease:Bounce.easeOut,
+			onComplete: animFooterText});
+	}
+
+	function animFooterText() {
+		TweenMax.to(footerText, 0.7, {scaleY: 2 scaleY: 2, ease:Bounce.easeInOut});
+	}
+
+ 	// animImage();
+
+ 	badge.addEventListener('mouseover', animImage);
+
+})();
