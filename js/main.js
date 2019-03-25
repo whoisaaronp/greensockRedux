692fe2a0cc01ffd4eedf9bb14d4b525e51bec2f6
Paterson <a_paterson@tss-hr420-dm12.fc.ca>
2019-03-25 13:45:05 -0400

    update

--- js/main.js
+++ js/main.js
@@ -2,22 +2,30 @@
 // 
 // console.log( fired);
 
+// (() => {
+
+// 	const 	badge = document.querySelector('img'),
+// 			footerText = document.querySelector('h3');
+
+// 	function animImage() {
+// 		TweenMax.to(badge, 1.2, {scaleY: 1.025, scaleY: 1.025, rotation: 50, ease:Bounce.easeOut,
+// 			onComplete: animFooterText});
+// 	}
+
+// 	function animFooterText() {
+// 		TweenMax.to(footerText, 0.7, {scaleY: 2 scaleY: 2, ease:Bounce.easeInOut});
+// 	}
+
+//  	// animImage();
+
+//  	badge.addEventListener('mouseover', animImage);
+
+// })();
+// 
 (() => {
 
-	const 	badge = document.querySelector('img'),
-			footerText = document.querySelector('h3');
+	let mytl = new TimelineLite();
 
-	function animImage() {
-		TweenMax.to(badge, 1.2, {scaleY: 1.025, scaleY: 1.025, rotation: 50, ease:Bounce.easeOut,
-			onComplete: animFooterText});
-	}
-
-	function animFooterText() {
-		TweenMax.to(footerText, 0.7, {scaleY: 2 scaleY: 2, ease:Bounce.easeInOut});
-	}
-
- 	// animImage();
-
- 	badge.addEventListener('mouseover', animImage);
+	myTl.to("#background")
 
 })();
